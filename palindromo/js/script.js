be39f06a87b9f2751424fbd45407788be7441542
RooRsmoka <chri.disa96@gmail.com>
2021-12-16 15:36:24 +0100

    creazione file e commenti sviluppo

---
+++ palindromo/js/script.js
@@ -0,0 +1,8 @@
+// Palindromo:
+// Chiedere all’utente di inserire una parola, Creare una funzione per capire se
+// la parola inserita è un palindromo.
+
+// Chiedo all'utente di inserire una parola.
+// Creo una funzione che determina sè la sequenza di caratteri che inserisce l'utente,
+// rimane invariata se letta al contrario.
+// Se la sequenza è invariata stampo il palindromo altrimenti stampo un messaggio.
---
+++ pari&dispari/js/script.js
@@ -0,0 +1,17 @@
+// Pari e Dispari:
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri.
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+// Dichiariamo chi ha vinto.
+
+// Chiediamo all'utente di scegliere tra pari e dispari, e dopo gli chiediamo
+// di inserire un numero da 1 a 5.
+
+// Creo una funzione che genera un numero random, da 1 a 5 per il computer.
+
+// Sommo i numeri.
+
+// Creo una funzione che stabilisce se la somma è pari o dispari.
+
+// Dichiariamo il vincitore.
