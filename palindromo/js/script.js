050c267589cc76d4099963ae4ce9f12aeb77bed9
RooRsmoka <chri.disa96@gmail.com>
2021-12-18 15:04:37 +0100

    completamento esercizio e correzione

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -2,7 +2,30 @@
 // Chiedere all’utente di inserire una parola, Creare una funzione per capire se
 // la parola inserita è un palindromo.
 
+// Creo una funzione che determina sè la sequenza di caratteri inserita dall'utente
+// rimane invariata se letta al contrario (palindromo).
+function ispalindrome(word) {
+
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+
+    // return reverse === word;
+
+    if (reverseWord === word) {
+        return true;
+    }
+
+    return false;
+}
 // Chiedo all'utente di inserire una parola.
-// Creo una funzione che determina sè la sequenza di caratteri che inserisce l'utente,
-// rimane invariata se letta al contrario.
-// Se la sequenza è invariata stampo il palindromo altrimenti stampo un messaggio.
+const userWord = prompt('Inserisci una parola a tua scelta.');
+// Creo la variabile e salvo l'elemento in cui stampero un messaggio.
+let outputHtml = document.querySelector('.output')
+// Stampo nel documento un messaggio a seconda del risultato.
+if (ispalindrome(userWord)) {
+    outputHtml.innerHTML = `La parola ${userWord} è un palindromo.`
+} else {
+    outputHtml.innerHTML = `La parola ${userWord} non è un palindromo.`
+}
--- pari&dispari/js/script.js
+++ pari&dispari/js/script.js
@@ -5,13 +5,46 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
 
+// Creo una funzione che genera un numero random, da 1 a 5 per il computer.
+function RandomNum(minNum, maxNum) {
+    const result =  Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
+    return result;
+}
+// Creo una funzione che stabilisce se la somma è pari o dispari.
+function isEven (num) {
+    // if (num % 2 === 0) {
+    //     return true;
+    // }
+    // return false;
+    return num % 2 === 0;
+}
+
+function userWin(totalNum, userSelection) {
+    return (isEven(totalNum) && userSelection === 'pari') || (!isEven(totalNum) && userSelection === 'dispari');
+}
 // Chiediamo all'utente di scegliere tra pari e dispari, e dopo gli chiediamo
 // di inserire un numero da 1 a 5.
-
-// Creo una funzione che genera un numero random, da 1 a 5 per il computer.
-
+const userChoice = prompt('scegli pari o dispari');
+const userNum = parseInt(prompt('Scegli un numero da 1 a 5.'));
+// Salvo nella variabile il numero random del computer.
+const computerNum = RandomNum(1, 5);
 // Sommo i numeri.
-
-// Creo una funzione che stabilisce se la somma è pari o dispari.
-
+const sum = userNum + computerNum;
+console.log(computerNum);
+console.log(sum);
 // Dichiariamo il vincitore.
+// if (isEven(sum) && userChoice === 'pari') {
+//     console.log('Hai vinto!');
+// } else if (isEven(sum) && userChoice !== 'pari') {
+//     console.log('Hai perso!');
+// } else if (!isEven(sum) && userChoice === 'dispari') {
+//     console.log('Hai vinto!');
+// } else {
+//     console.log('Hai perso!');
+// }
+
+if(userWin()) {
+    console.log('Hai vinto!');
+} else {
+    console.log('Hai perso!');
+}
